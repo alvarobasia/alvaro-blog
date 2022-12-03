7f19c5d542ec058dd5a3fbe9a60ccdae428f019d
Álvaro Araújo <alvaro.araujo@aluno.ufop.edu.br>
2022-12-03 15:17:57 -0300

    refactor: remove unused dependency

--- src/components/base/Avatar.tsx
+++ src/components/base/Avatar.tsx
@@ -1,15 +1,20 @@
 import Image from 'next/image';
-import {useState} from "react"
 
 interface AvatarProps {
-    src: string;
+  src: string;
 }
 
-const Avatar: React.FC<AvatarProps> = (props:AvatarProps) => {
-
-  return <div className='overflow-hidden w-[100px] h-[100px] rounded-full bg-slate-700 border-2 border-white'>
-    <Image src={props.src} width={100} height={100} alt="Imagem do desenvolvedor do blog"/>
-  </div>;
+const Avatar: React.FC<AvatarProps> = (props: AvatarProps) => {
+  return (
+    <div className="overflow-hidden w-[100px] h-[100px] rounded-full bg-slate-700 border-2 border-white">
+      <Image
+        src={props.src}
+        width={100}
+        height={100}
+        alt="Imagem do desenvolvedor do blog"
+      />
+    </div>
+  );
 };
 
 export default Avatar;
