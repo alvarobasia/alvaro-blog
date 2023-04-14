7c30a7786f73018537ff8907f2d9c4c18342c419
Álvaro Araújo <alvaro.araujo@aluno.ufop.edu.br>
2023-04-14 01:02:59 -0300

    chore: remove graphql and dependencies

--- src/components/elements/BlogHome.tsx
+++ src/components/elements/BlogHome.tsx
@@ -1,10 +1,25 @@
 'use client';
-import {Post, useGetPostsQuery} from '../../graphql/generated';
+
 import PostCard from '../complex/PostCard';
 
+export interface Post {
+  id: number;
+  date: Date;
+  excerpt: string;
+  title: string;
+  tags: string[];
+}
+
 const BlogHome: React.FC = () => {
-  const {data} = useGetPostsQuery();
-  const posts = data?.posts || [];
+  const posts: Post[] = [
+    {
+      id: 1,
+      date: new Date(),
+      excerpt: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+      title: 'Lorem ipsum dolor sit amet',
+      tags: ['tag1', 'tag2', 'tag3'],
+    },
+  ];
   return (
     <div className="flex-1 bg-gray-800 p-20 overflow-scroll overflow-x-hidden h-[100%]">
       <ul className="flex flex-col gap-11">
